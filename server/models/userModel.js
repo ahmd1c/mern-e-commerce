4b6adc3d685cb2b9528a3e9a8913a7ec2c8c912e
ahmd1c <gwerqweldiv@gmail.com>
2023-09-24 19:17:26 +0300

    added userModel

---
+++ server/models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: [true, "username is required"],
+            maxLength: [20, "Too long username"],
+            minLength: [5, "Too short username"],
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"],
+            maxLength: [25, "Too long password"],
+            minLength: [8, "Too short password"],
+        },
+        email: {
+            type: String,
+            required: [true, "email is required"],
+            maxLength: [32, "Too long email"],
+            minLength: [8, "Too short email"],
+            unique: true,
+            trim: true,
+            validate: {
+                validator: function (email) {
+                    return validator.isEmail(email);
+                },
+                message: (props) => `${props.value} is not a valid email`,
+            },
+        },
+        whishlist: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            },
+        ],
+        profilePhoto: {
+            type: String,
+            default: "profile.png",
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 11);
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
