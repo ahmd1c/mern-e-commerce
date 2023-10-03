a77250e601074814cb5b774ad4646dab50a7c66f
ahmd1c <gwerqweldiv@gmail.com>
2023-10-03 05:39:45 +0300

    modified header component

--- client/src/mainComponents/Header.jsx
+++ client/src/mainComponents/Header.jsx
@@ -1,23 +1,33 @@
+import { useState } from "react"
 import NavIcons from "./NavIcons"
 import NavLinks from "./NavLinks"
 import Search from "./Search"
+import UserNav from "./UserNav"
 // import {Link} from "react-router-dom"
 import "./header.css"
 
 function Header() {
+    const [active , setActive] = useState(false)
+
+    const handleAsideNav = ()=>{
+        setActive(!active)
+    }
+
     return (
         <header>
             <div className="upper-section">
-                <a href="/" >Ein-c</a>
+                <div onClick={handleAsideNav} className={`menu-icon ${active ? "active" : null}`}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+                <a className="logo" href="/" >Ein-c</a>
                 <Search />
                 <NavIcons />
-                <div className="user">
-                    <button>Login</button>
-                    <button>Register</button>
-                </div>
+                <UserNav />
 
             </div>
-            <NavLinks />
+                <NavLinks active={active} handleAsideNav={handleAsideNav} />
         </header>
     )
 }
