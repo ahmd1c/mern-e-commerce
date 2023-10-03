6a7326ad1ae3ffb39c663082faf37aec3b35bd61
ahmd1c <gwerqweldiv@gmail.com>
2023-10-03 05:37:34 +0300

    added search component ui

---
+++ client/src/mainComponents/Search.jsx
@@ -0,0 +1,17 @@
+
+import "./search.css"
+
+
+
+function Search() {
+    return (
+        <div  className="search">
+            <input  type="text" placeholder="Search" />
+            <span className="material-symbols-outlined">
+                search
+            </span>
+        </div>
+    )
+}
+
+export default Search
