b1586bbb6c6278d11f0114a780a15fff6a4af3bb
ahmd1c <gwerqweldiv@gmail.com>
2023-10-03 05:36:46 +0300

    added user nav ui

---
+++ client/src/mainComponents/UserNav.jsx
@@ -0,0 +1,12 @@
+
+
+function UserNav() {
+    return (
+        <div className="user">
+            <button>Login</button>
+            <button>Register</button>
+        </div>
+    )
+}
+
+export default UserNav
