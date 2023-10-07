fec6aa5e57cb503a9cd66801d7b90bc48af90cb1
ahmd1c <gwerqweldiv@gmail.com>
2023-10-07 17:03:23 +0300

    added the structure of react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 
 import './App.css'
+import { createBrowserRouter , RouterProvider , createRoutesFromElements , Route } from "react-router-dom"
+import Home from './pages/home/Home'
+import Products from './pages/products/Products'
+import RootLayout from './mainComponents/appLayout/RootLayout'
 
 function App() {
   
+  const Router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/"  element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="products" element={<Products />} />
+    </Route>
+  ))
 
   return (
     <>
-      
+      <RouterProvider router={Router}  />
     </>
   )
 }
