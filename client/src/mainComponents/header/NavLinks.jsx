a59597e48392093701fc683839e54856c4ed604f
ahmd1c <gwerqweldiv@gmail.com>
2023-10-08 17:07:33 +0300

    added sub menu for categories

--- client/src/mainComponents/header/NavLinks.jsx
+++ client/src/mainComponents/header/NavLinks.jsx
@@ -1,24 +1,37 @@
+import { NavLink } from "react-router-dom"
 import UserNav from "./UserNav"
 import "./css/navlinks.css"
+import { useState } from "react"
 
 // eslint-disable-next-line react/prop-types
-function NavLinks({handleAsideNav , active}) {
+function NavLinks({ handleAsideNav, active }) {
 
-
+  const [categList , setCategList] = useState(false)
 
   return (
-    <nav className={active ? "active" : null }>
+    <nav className={active ? "active" : null}>
       <div onClick={handleAsideNav} className={`menu-icon ${active ? "active" : null}`}>
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
       </div>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="">Products</a></li>
-        <li><a href="">Categories</a></li>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/products">Products</NavLink></li>
+        <li onClick={()=> setCategList((prev)=> !prev)} className="nav-category-item"><span className="nav-main-span">Categories <span className="material-symbols-outlined">
+          arrow_drop_down
+        </span></span>
+          <ul className={`nav-category-list ${categList ? "active" : ""}`}>
+            <li><NavLink to="/categories">Category 1</NavLink></li>
+            <li><NavLink to="/categories">Category 2</NavLink></li>
+            <li><NavLink to="/categories">Category 3</NavLink></li>
+            <li><NavLink to="/categories">Category 4</NavLink></li>
+            <li><NavLink to="/categories">Category 5</NavLink></li>
+          </ul>
+        </li>
       </ul>
       <UserNav />
+
     </nav>
   )
 }
