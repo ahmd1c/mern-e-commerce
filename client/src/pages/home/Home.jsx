9d085628f0a58a8eb06352fcf3ca42fa7269b6fe
ahmd1c <gwerqweldiv@gmail.com>
2023-10-15 22:53:18 +0300

    modified home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,22 +1,50 @@
 
 import './home.css'
 import image from "../../assets/svg.svg"
+import TopRatedProducts from './pageComponent/TopRatedProducts'
+import { useEffect, useState } from 'react'
+import MostSelledProducts from './pageComponent/MostSelled'
 
 function Home() {
+  const [topProducts, setTopProducts] = useState([])
+
+  
+    // useEffect(() => {
+    //     fetch('https://fakestoreapi.com/products?limit=10')
+    //         .then(res => res.json())
+    //         .then(data => {
+
+    //           setTopProducts(data);
+    //             console.log(data);
+
+    //         })
+    // }, [])
   return (
-    <main>
+    <>
       <div className="hero">
         <div className="hero-text">
           <h1> Experience Your Gateway to Quality Shopping 
           Enjoy our unbeatable offers </h1>
           <p> Your perfect product is just a click away.</p>
-          <button>Shop now</button>
+          <button className='btn-primary'>Shop now</button>
         </div>
         <div className="hero-img">
           <img src={image} alt='' />
         </div>
       </div>
-    </main>
+      {topProducts.length>0 ?
+      (
+      <>
+      <TopRatedProducts list={topProducts} />
+      <br />
+      <br />
+      <br />
+      <MostSelledProducts list={topProducts} />
+      </>
+      )
+      :<h1>some thing went wrong</h1>
+      }
+    </>
   )
 }
 
