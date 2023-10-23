1db1bd8ead2bfa77ad57c9e6c9c1c08b9017b6f3
ahmd1c <gwerqweldiv@gmail.com>
2023-10-23 16:14:06 +0300

    added user reviews component

---
+++ client/src/pages/user/reviews/Reviews.jsx
@@ -0,0 +1,150 @@
+import "./reviews.css"
+import IMAGE from "../../../assets/modified.webp"
+
+function Reviews() {
+  return (
+    <div className="reviews-cont">
+      <table>
+        <thead>
+          <tr>
+            <th><input className="table-input" type="checkbox" name="" id="" /></th>
+            <th>product</th>
+            <th>rate</th>
+            <th>message</th>
+            <th>date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+          <tr>
+            <td><input className="table-input" type="checkbox" name="" id="" /></td>
+            <td>
+              <div>
+                <div className="product-img">
+                <img src={IMAGE} alt="" />
+                </div>
+                <h4>name</h4>
+              </div>
+            </td>
+            <td>4/5
+              <span className="material-symbols-outlined">
+                edit_note
+              </span>
+            </td>
+            <td><h5 className="text-overflow">uieqwSDSDSDSDSDSDiueyqwiueyqwiueyqwiu2eyqwiu2dddddddddddddddddddddddde</h5><button>view</button></td>
+            <td>23/4/2023</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Reviews
