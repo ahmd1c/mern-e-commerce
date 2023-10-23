479d973f5c9af3f47d314b6e90a47e13708c23e3
ahmd1c <gwerqweldiv@gmail.com>
2023-10-23 16:13:08 +0300

    added whishlist component

---
+++ client/src/pages/user/whishlist/WhishList.jsx
@@ -0,0 +1,12 @@
+import ProductList from "../../products/pageComponents/ProductsList"
+import "./whishList.css"
+
+function WhishList() {
+  return (
+    <div className="whish-cont">
+      <ProductList />
+    </div>
+  )
+}
+
+export default WhishList
