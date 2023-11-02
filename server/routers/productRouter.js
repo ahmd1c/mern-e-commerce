6c18fd59a928479fb9e49e69518231283a3e668c
ahmd1c <gwerqweldiv@gmail.com>
2023-11-02 22:23:43 +0200

    added product router

---
+++ server/routers/productRouter.js
@@ -0,0 +1,15 @@
+const { getProducts, getProduct, updateProduct, deleteProduct, createProduct } = require("../controllers/productController");
+const { verifyAdmin } = require("../utils/authVerification");
+const uploadPhoto = require("../utils/uploadPhoto");
+const {  assertCategoryandSubCategory, createProductValidator, updateProductValidator }  = require("../validation/productValidation");
+
+const router = require("express").Router();
+
+router.get("/", getProducts)
+router.post("/" , verifyAdmin , uploadPhoto.single("cover") , createProductValidator , assertCategoryandSubCategory ,createProduct)
+router.get("/:productId", getProduct)
+router.put("/:productId", verifyAdmin , uploadPhoto.single("cover") , updateProductValidator , assertCategoryandSubCategory ,  updateProduct)
+router.delete("/:productId", verifyAdmin , deleteProduct)
+
+
+module.exports = router;
