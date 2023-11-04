b31699064ff9fc921afd84cc6a14584683c82843
ahmd1c <gwerqweldiv@gmail.com>
2023-11-04 23:11:57 +0200

    added some routers

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config({ path: "./config.env" });
 const cors = require("cors");
 const { errorHandler, notFound } = require("./utils/errorHandler");
 const userRouter = require("./routers/userRouter")
+const productRouter = require("./routers/productRouter")
+const categoryRouter = require("./routers/categoryRouter")
+const cartRouter = require("./routers/cartRouter")
+
+const cookieParser = require("cookie-parser")
+
+
 
 const app = express();
 
@@ -19,10 +26,15 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 //   console.log(`server is running on port ${process.env.PORT}`);
 // })
 
-app.use(cors({credentials : true , origin : "http://localhost:3000"}))
+app.use(cors({credentials : true , origin : "http://localhost:5173"}))
 app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(cookieParser())
 
 app.use("/api/v1/user" , userRouter)
+app.use("/api/v1/product" , productRouter)
+app.use("/api/v1/category" , categoryRouter)
+app.use("/api/v1/cart" , cartRouter)
 
 app.use(notFound)
 app.use(errorHandler)
