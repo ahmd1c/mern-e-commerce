9862b5bcfe92ca79827d3c1b9303f24d5b55b35d
ahmd1c <gwerqweldiv@gmail.com>
2023-11-05 23:31:47 +0200

    added id error handler

--- server/utils/errorHandler.js
+++ server/utils/errorHandler.js
@@ -2,7 +2,8 @@
 exports.errorHandler = (err, req, res, next) => {
 
     let customError = {};
-    // console.log(err);
+    console.log("came here");
+    console.log(err);
     // check if the error is a joi validation error which have an _original property 
 
     if (err._original) {
@@ -16,10 +17,18 @@ exports.errorHandler = (err, req, res, next) => {
     // check if the error is a mongoose validation error and not a joi validation error as joi validation error has a _original property
 
     if ((err.name === 'CastError' || err.name === 'ValidationError') && err._original == undefined) {
+        console.log("PRESENT");
         Object.values(err.errors).forEach((error) => {
             customError[error.path] = error.message
         })
         return res.status(400).json(customError)
+    }
+
+    if (err.message === "invalid id"){
+        return res.status(404).json({
+            success: false,
+            message: "invalid id"
+        })
     }
 
     customError.message = err.message;
