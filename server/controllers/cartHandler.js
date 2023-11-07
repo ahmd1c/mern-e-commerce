27ab8f774ced49f00fbbe7e19564726888f3a6d8
ahmd1c <gwerqweldiv@gmail.com>
2023-11-07 15:45:05 +0200

    added cart handler

---
+++ server/controllers/cartHandler.js
@@ -0,0 +1,102 @@
+
+const Cart = require("../models/cartModel");
+const Product = require("../models/productModel");
+const asyncHandler = require("../utils/asyncHandler");
+
+exports.addToCart = asyncHandler( async (req, res) => {
+
+    // GET THE PRODUCT ID FROM THE BODY
+    const { productId, quantity } = req.body;
+
+    // CHECK WHETHER THE PRODUCT IS AVAILABLE IN THE STORE OR NOT
+    const product = await Product.findById(productId);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
+    if (product.amount < quantity) {
+        return res.status(400).json({
+            success: false,
+            message: product.amount > 0 ? `Only ${product.amount} left` : "Product out of stock"
+        });
+    }
+
+    // CHECK WHEHTER THE USER HAS A CART OR NOT
+    let userCart = await Cart.findOne({ userId: req.user.id });
+
+    // CREATE A NEW CART IF THERE IS NO CART
+    if (!userCart) {
+        userCart = await Cart.create({
+            userId: req.user.id,
+            productsList: [
+                {
+                    productId,
+                    quantity
+                }
+            ],
+            totalPrice: quantity * product.afterPrice
+        })
+
+    } else {
+        // CHECKS IF THE PRODUCT IS ALREADY IN THE CART
+        const productExist = userCart.productsList.find(
+            (p) => p.productId.toString() === productId.toString()
+        );
+        // IF PRODUCT EXISTS THEN INCREMENT QUANTITY & TOTAL PRICE 
+        if (productExist) {
+            productExist.quantity += quantity;
+            userCart.totalPrice += quantity * product.afterPrice;
+
+        } else {
+            // IF PRODUCT DOES NOT EXIST THEN ADD THE PRODUCT TO THE CART
+            userCart.productsList.push({
+                productId,
+                quantity
+            });
+            userCart.totalPrice += quantity * product.afterPrice;
+        }
+
+        await userCart.save();
+
+    }
+
+    res.status(200).json({
+        success: true,
+        userCart
+    })
+
+})
+
+exports.getCart = asyncHandler( async (req, res) => {
+
+    const userCart = await Cart.findOne({ userId: req.user.id }).populate("productsList.productId");
+
+    if (!userCart) {
+        return res.status(404).json({
+            success: false,
+            message: "Cart not found"
+        });
+    }
+
+    return res.status(200).json({
+        success: true,
+        userCart
+    })
+
+})
+
+
+
+
+exports.clearCart = asyncHandler( async (req, res) => {
+    await Cart.findOneAndDelete({ userId: req.user.id });
+    res.status(200).json({
+        success: true,
+        message: "Cart cleared successfully"
+    })
+})
+
