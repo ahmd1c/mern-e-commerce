0d7ed4aab3b90a42cfa6488df3e0e22cd656891f
ahmd1c <gwerqweldiv@gmail.com>
2023-11-13 20:00:17 +0200

    added the store and provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './redux/stores.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* wrap the app with redux store */}
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
