9e2a5755faa3589f89858539944f9bd3fcd28d16
ahmd1c <gwerqweldiv@gmail.com>
2023-11-14 23:31:35 +0200

    added user links

--- client/src/mainComponents/header/UserNav.jsx
+++ client/src/mainComponents/header/UserNav.jsx
@@ -1,16 +1,40 @@
 import { NavLink } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { logUserOut } from "../../redux/userReducer";
+import "./css/userNav.css"
 
-
+// eslint-disable-next-line react/prop-types
 function UserNav() {
+
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
+    const handleLogout = () => {
+        // fetch logout
+        dispatch(logUserOut());
+    }
+
     return (
-        <div className="user">
-            <button>
-                <NavLink to="/login">login</NavLink>
-            </button>
-            <button>
-                <NavLink to="/register">register</NavLink>
-            </button>
-        </div>
+        // eslint-disable-next-line react/prop-types
+        user?.email ? (
+            <div className="user">
+                
+                <NavLink to="/user">
+                    <h5>{user?.username}ERERERERERERER</h5>
+                    <img src={user?.profilePhoto} alt="profile" />
+                </NavLink>
+                <button onClick={handleLogout}>
+                    logout
+                </button>
+            </div>
+        ) :
+            <div className="user">
+                <button>
+                    <NavLink to="/login">login</NavLink>
+                </button>
+                <button>
+                    <NavLink to="/register">register</NavLink>
+                </button>
+            </div>
     )
 }
 
