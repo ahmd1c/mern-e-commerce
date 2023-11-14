78bf0eb31880a07915ea7362b2397a942c20486f
ahmd1c <gwerqweldiv@gmail.com>
2023-11-14 23:29:50 +0200

    ADDED PROTECTED ROUTES

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { createBrowserRouter , RouterProvider , createRoutesFromElements , Route } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom"
 import Home from './pages/home/Home'
 import Products from './pages/products/Products'
 import RootLayout from './mainComponents/appLayout/RootLayout'
@@ -9,24 +9,39 @@ import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Dashboard from './pages/Admin/Dashboard/Dashboard'
 import Settings from './pages/user/settings/Settings'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUser } from './redux/userReducer'
 
 function App() {
-  
+
+  const dispatch = useDispatch();
+  dispatch(getUser());
+  const user = useSelector((state) => state.user);
+
+
   const Router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/"  element={<RootLayout />}>
+    <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="products" element={<Products />} />
       <Route path="product/:id" element={<Product />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<Dashboard />} />
-      <Route path="/user" element={<Settings />} />
+      {
+        user.role === "admin" && 
+        (
+          <Route path="/admin" element={<Dashboard />} />
+        )  
+      }
+      {user.email && 
+      (
+        <Route path="/user" element={<Settings />} />
+      )}
     </Route>
   ))
 
   return (
     <>
-      <RouterProvider router={Router}  />
+      <RouterProvider router={Router} />
     </>
   )
 }
