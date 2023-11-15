ad9700b8801a0f5758d7b06fe8b7098eb2013bf1
ahmd1c <gwerqweldiv@gmail.com>
2023-11-15 20:40:51 +0200

    added number of items to cart icon in the header

--- client/src/mainComponents/header/NavIcons.jsx
+++ client/src/mainComponents/header/NavIcons.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable react/prop-types */
 
+import { useSelector } from "react-redux";
 import "./css/navicons.css"
 
 function NavIcons({ setCartActive}) {
+
+    const cartTotal = useSelector((state) => state.cart.cartTotal);
+
     return (
         
         <div className="icons">
             <span data-amount={0} className="material-symbols-outlined">
                 favorite
             </span>
-            <span onClick={()=> setCartActive(true)} data-amount={0} className="material-symbols-outlined">
+            <span onClick={()=> setCartActive(true)} data-amount={cartTotal} className="material-symbols-outlined">
                 shopping_cart
             </span>
         </div>
