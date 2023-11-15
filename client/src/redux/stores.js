7bf25b23c63a61695180ed03d656472bd6faf92b
ahmd1c <gwerqweldiv@gmail.com>
2023-11-15 20:39:31 +0200

    added redux store

---
+++ client/src/redux/stores.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userReducer";
+import cartReducer from "./cartReducer";
+
+export const store = configureStore({
+    reducer: { 
+        user: userReducer,
+        cart: cartReducer 
+    },
+});
