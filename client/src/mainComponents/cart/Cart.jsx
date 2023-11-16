8d67c2ba3e1b79e84ca265d1efdda8262a28aee4
ahmd1c <gwerqweldiv@gmail.com>
2023-11-16 23:11:16 +0200

    added cart functionality

--- client/src/mainComponents/cart/Cart.jsx
+++ client/src/mainComponents/cart/Cart.jsx
@@ -1,24 +1,45 @@
 /* eslint-disable react/prop-types */
+import { useDispatch, useSelector } from "react-redux"
 import CartItem from "../cartItem/cartItem"
 import "./cart.css"
+import { clearCart } from "../../redux/cartReducer";
 
-function Cart({cartActive , setCartActive}) {
+function Cart({ cartActive, setCartActive }) {
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart);
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    }
+
     return (
         <div className={`cart-aside ${cartActive ? "active" : ""}`}>
-            <span className="material-symbols-outlined" onClick={()=> setCartActive(false)} >close</span>
-            <h1>Cart</h1>
-            <div className="cart-items">
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
+            <span className="material-symbols-outlined" onClick={() => setCartActive(false)} >close</span>
+            <div className="upper">
+                <h1>Cart</h1>
+                <h3>{cart.cartTotal} items</h3>
+                <button onClick={handleClearCart}>Clear</button>
             </div>
-            <div className="total">total</div>
-            <button style={{width : "100%"}} className="btn-primary">Checkout</button>
+            <div className="cart-items">
+
+                {cart.cartItems.length === 0 ? (
+                    <h3>Your cart is empty</h3>
+                ) : (
+                    cart.cartItems.map((item) => <CartItem key={item.id} item={item} />)
+                )}
+            </div>
+
+            <form className="coupon">
+                <input type="text" placeholder="Coupon code" />
+                <button>Apply</button>
+            </form>
+
+            <div className="total">
+                <h3>Total</h3>
+                <h3>{cart.cartTotalCost} $</h3>
+            </div>
+            <button style={{ width: "100%" }} className="btn-primary">Checkout</button>
         </div>
     )
 }
