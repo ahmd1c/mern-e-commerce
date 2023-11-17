1367f5add538d0b6ecc4ed301c07dba2e0e65edb
ahmd1c <gwerqweldiv@gmail.com>
2023-11-17 23:16:15 +0200

    added redux function for cart item

--- client/src/mainComponents/cartItem/CartItem.jsx
+++ client/src/mainComponents/cartItem/CartItem.jsx
@@ -1,24 +1,43 @@
+/* eslint-disable react/prop-types */
 
+import { useDispatch } from "react-redux"
 import "./cartItem.css"
+import { addCartItem, decreaseCartItem, deleteCartItem } from "../../redux/cartReducer"
 
-function CartItem() {
+function CartItem({ item }) {
+    const dispatch = useDispatch()
+    const handleIncrease = () => {
+        dispatch(addCartItem(item))
+    }
+
+    const handleDecrease = () => {
+        dispatch(decreaseCartItem(item.id))
+    }
+
+    const handleDelete = () => {
+        dispatch(deleteCartItem(item.id))
+    }
+
     return (
         <div className="cart-item">
             <div className="cart-item-left-side">
                 <div className="cart-item-img">
-                    <img src="" alt="" />
+                    <img src={item.image} alt="item" />
                 </div>
                 <div className="cart-item-info">
-                    <h5>title</h5>
+                    <h5>{item.title}</h5>
                     <div className="item-cart-amount">
-                        <button>-</button>
-                        <input type="number" name="" id="" />
-                        <button>+</button>
+                        <button onClick={() => handleDecrease()}>-</button>
+                        <input value={item.quantity} type="number" name="quantity" id="" />
+                        <button onClick={() => handleIncrease()}>+</button>
                     </div>
-                    <h4>price</h4>
+                    <div className="item-cart-price">
+                        <h4>{item.price} $</h4>
+                        <h5>X {item.quantity}</h5>
+                    </div>
                 </div>
             </div>
-            <span className="material-symbols-outlined">
+            <span onClick={() => handleDelete()} className="material-symbols-outlined">
                 delete
             </span>
         </div>
