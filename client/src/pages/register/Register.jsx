f425898a39d8aac897093fe201dddd29846a738e
ahmd1c <gwerqweldiv@gmail.com>
2023-11-22 19:11:16 +0200

    ADDED REGISTER FUNCTIONALITY

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,31 +1,98 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../redux/userReducer";
+import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 function Register() {
-  return (
-    <div className="auth-cont">
-            <form >
-            <div className="name">
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatPassword, setrepeatPassword] = useState("");
+
+    const handleSignUpSubmit = async (e) => {
+        e.preventDefault();
+        if (
+            username.trim() === "" ||
+            email.trim() === "" ||
+            password.trim() === "" ||
+            password !== repeatPassword
+        )
+            return toast.error("please fill the required fields");
+
+        try {
+            const response = await fetch("http://localhost:5000/api/v1/user/signup", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username ,
+                    email ,
+                    password,
+                    repeatPassword,
+                }),
+            });
+            const data = await response.json();
+            console.log(data);
+            if (data.success === true) {
+                dispatch(setUser(data.user));
+                toast.success("You are successfully signed up");
+                navigate("/", { replace: true });
+            }
+        } catch (error) {
+            toast.error("something went wrong");
+        }
+    };
+    return (
+        <div className="auth-cont">
+            <ToastContainer />
+            <form onSubmit={handleSignUpSubmit}>
+                <div className="name">
                     <label htmlFor="name">name</label>
-                    <input type="text" name="name" id="name" />
+                    <input
+                        onChange={(e) => setUsername(e.target.value)}
+                        type="text"
+                        name="name"
+                        id="name"
+                    />
                 </div>
                 <div className="email">
                     <label htmlFor="email">email</label>
-                    <input type="email" name="email" id="email" />
+                    <input
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        name="email"
+                        id="email"
+                    />
                 </div>
                 <div className="password">
                     <label htmlFor="password">password</label>
-                    <input type="password" name="password" id="password" />
+                    <input
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        name="password"
+                        id="password"
+                    />
                 </div>
                 <div className="repeat-password">
                     <label htmlFor="repeat-password">repeat password</label>
-                    <input type="password" name="repeat-password" id="repeat-password" />
+                    <input
+                        onChange={(e) => setrepeatPassword(e.target.value)}
+                        type="password"
+                        name="repeat-password"
+                        id="repeat-password"
+                    />
                 </div>
                 <div className="forget-register">
-                    
                     <span>
-                        Have an account ?
-                        <Link to="/login">login now</Link>
+                        Have an account ?<Link to="/login">login now</Link>
                     </span>
                     <hr />
                     <button type="button">Login with google</button>
@@ -34,7 +101,7 @@ function Register() {
                 </div>
             </form>
         </div>
-  )
+    );
 }
 
-export default Register
+export default Register;
