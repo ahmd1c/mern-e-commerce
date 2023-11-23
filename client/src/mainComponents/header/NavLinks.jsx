99b176d4e4d2e428bb4f552ec2fb8871c835f003
ahmd1c <gwerqweldiv@gmail.com>
2023-11-23 23:42:06 +0200

    added product category routing

--- client/src/mainComponents/header/NavLinks.jsx
+++ client/src/mainComponents/header/NavLinks.jsx
@@ -1,13 +1,25 @@
 import { NavLink } from "react-router-dom"
 import UserNav from "./UserNav"
 import "./css/navlinks.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
+import { toast } from "react-toastify"
 
 // eslint-disable-next-line react/prop-types
 function NavLinks({ handleAsideNav, active }) {
   const user = useSelector((state) => state.user)
-  const [categList, setCategList] = useState(false)
+  const [categListShow, setCategListShow] = useState(false)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await fetch("http://localhost:5000/api/v1/category")
+      if (!res.ok) return toast.error("something went wrong , please try again")
+      const data = await res.json()
+      setCategories(data.categories)
+    }
+    getCategories()
+  }, [])
 
   return (
     <nav className={active ? "active" : null}>
@@ -18,20 +30,17 @@ function NavLinks({ handleAsideNav, active }) {
       </div>
       <ul>
         <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/products">Products</NavLink></li>
         <li
           onClick={(e) => {
-            if (e.target.nodeName === "SPAN") setCategList((prev) => !prev)
+            if (e.target.nodeName === "SPAN") setCategListShow((prev) => !prev)
           }}
-          className="nav-category-item"><span className="nav-main-span">Categories <span className="material-symbols-outlined">
+          className="nav-category-item"><span className="nav-main-span">Products <span className="material-symbols-outlined">
             arrow_drop_down
           </span></span>
-          <ul className={`nav-category-list ${categList ? "active" : ""}`}>
-            <li><NavLink to="/categories">Category 1</NavLink></li>
-            <li><NavLink to="/categories">Category 2</NavLink></li>
-            <li><NavLink to="/categories">Category 3</NavLink></li>
-            <li><NavLink to="/categories">Category 4</NavLink></li>
-            <li><NavLink to="/categories">Category 5</NavLink></li>
+          <ul className={`nav-category-list ${categListShow ? "active" : ""}`}>
+            {categories.map((category) => (
+              <li key={category._id}><NavLink  to={`/products?category=${category._id}`}>{category.name}</NavLink></li>
+            ))}
           </ul>
         </li>
 
