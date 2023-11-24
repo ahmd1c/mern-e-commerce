af57321f7f6fe4f57d446934193198487cfe5972
ahmd1c <gwerqweldiv@gmail.com>
2023-11-24 10:08:52 +0200

    rebuild cart model

--- server/models/cartModel.js
+++ server/models/cartModel.js
@@ -3,12 +3,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const cartSchema = new Schema({
-    productId: [{
+const productsListSchema = new Schema({
+    productId: {
         type: Schema.Types.ObjectId,
         ref: 'Product',
         required: true
-    }],
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [1, 'Quantity can not be less than 1'],
+        max: [50, 'Quantity can not be more than 50']
+    },
+}, { _id: false });
+
+const cartSchema = new Schema({
+    productsList: [productsListSchema],
     totalPrice: {
         type: Number,
         required: true
@@ -18,9 +28,9 @@ const cartSchema = new Schema({
         ref: 'User',
         required: true
     },
-    
 
-},{timestamps : true});
+
+}, { timestamps: true });
 
 const Cart = mongoose.model('Cart', cartSchema);
 
