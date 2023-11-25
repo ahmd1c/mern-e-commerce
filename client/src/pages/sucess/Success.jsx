982ebac582fec104471f047c1f8065394fd49fb9
ahmd1c <gwerqweldiv@gmail.com>
2023-11-25 07:35:21 +0200

    ADDED ORDER SUCCESS PAGE

---
+++ client/src/pages/sucess/Success.jsx
@@ -0,0 +1,39 @@
+import "./success.css"
+import correct from "../../assets/correct.webp"
+import { NavLink } from "react-router-dom"
+import { useEffect } from "react"
+import { ToastContainer, toast } from "react-toastify"
+
+function Success() {
+
+    useEffect(() => {
+        const handleSuccess = async () => {
+            const res = await fetch("http://localhost:5000/api/v1/order/success", {
+                method: "POST",
+                credentials: "include",
+            })
+            if (!res.ok) {
+                return toast.error("Something went wrong");
+            }
+            const data = await res.json();
+            if (data.success) {
+                toast.success(data.msg);
+                console.log(data);
+            }
+        }
+
+        handleSuccess();
+
+    }, [])
+
+    return (
+        <div className="success-cont">
+            <ToastContainer />
+            <h1>Order Placed Successfully</h1>
+            <img src={correct} alt="success" />
+            <NavLink to="/">Back To Home</NavLink>
+        </div>
+    )
+}
+
+export default Success
