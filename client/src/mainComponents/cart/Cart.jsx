db1c3a05ed07fc308adfbe5332d4ac557dec7f0f
ahmd1c <gwerqweldiv@gmail.com>
2023-11-26 16:33:00 +0200

    ADDED CART FUNCTIONS

--- client/src/mainComponents/cart/Cart.jsx
+++ client/src/mainComponents/cart/Cart.jsx
@@ -3,18 +3,40 @@ import { useDispatch, useSelector } from "react-redux"
 import CartItem from "../cartItem/cartItem"
 import "./cart.css"
 import { clearCart } from "../../redux/cartReducer";
+import { ToastContainer, toast } from "react-toastify";
 
 function Cart({ cartActive, setCartActive }) {
 
     const dispatch = useDispatch();
 
     const cart = useSelector((state) => state.cart);
-    const handleClearCart = () => {
+    const handleClearCart = async() => {
+        const res = await fetch("http://localhost:5000/api/v1/cart", {
+            method : "DELETE",
+            credentials : "include"
+        })
+        if(!res.ok) {
+            return toast.error("Something went wrong");
+        }
+        toast.success("Cart cleared successfully");
         dispatch(clearCart());
+    }
+
+    const handleCheckout = async() => {
+        const res = await fetch("http://localhost:5000/api/v1/order/create-checkout-session", {
+            method : "POST",
+            credentials : "include",
+        })
+        if(!res.ok) {
+            return toast.error("Something went wrong");
+        }
+        const data = await res.json();
+        window.location.href = data.url;
     }
 
     return (
         <div className={`cart-aside ${cartActive ? "active" : ""}`}>
+            <ToastContainer />
             <span className="material-symbols-outlined" onClick={() => setCartActive(false)} >close</span>
             <div className="upper">
                 <h1>Cart</h1>
@@ -26,7 +48,7 @@ function Cart({ cartActive, setCartActive }) {
                 {cart.cartItems.length === 0 ? (
                     <h3>Your cart is empty</h3>
                 ) : (
-                    cart.cartItems.map((item) => <CartItem key={item.id} item={item} />)
+                    cart.cartItems.map((item) => <CartItem key={item._id} item={item} />)
                 )}
             </div>
 
@@ -39,7 +61,7 @@ function Cart({ cartActive, setCartActive }) {
                 <h3>Total</h3>
                 <h3>{cart.cartTotalCost} $</h3>
             </div>
-            <button style={{ width: "100%" }} className="btn-primary">Checkout</button>
+            <button onClick={handleCheckout} style={{ width: "100%" }} className="btn-primary">Checkout</button>
         </div>
     )
 }
