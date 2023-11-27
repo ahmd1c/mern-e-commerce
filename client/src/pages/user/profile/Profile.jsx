2ef5a2b85ad8ab475bb86d6d0d9634bdaa176ee7
ahmd1c <gwerqweldiv@gmail.com>
2023-11-27 18:00:23 +0200

    added user profile functionality

--- client/src/pages/user/profile/Profile.jsx
+++ client/src/pages/user/profile/Profile.jsx
@@ -1,26 +1,52 @@
+import { useRef } from "react";
 import "./profile.css"
-import image from '../../../assets/default.png'
+// import image from '../../../assets/default.png'
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify";
+import { setUser } from "../../../redux/userReducer";
 
 function Profile() {
+
+  const user = useSelector((state) => state.user);
+  const formRef = useRef(null)
+  const dispatch = useDispatch();
+  console.log(user);
+  const handleUpdate = async(e) => {
+    e.preventDefault();
+    const formData = new FormData(formRef.current);
+    const res = await fetch(`http://localhost:5000/api/v1/user/profile`, {
+        method: "PUT",
+        credentials: "include",
+        body: formData
+    })
+    if(!res.ok) return toast.error("Something went wrong");
+    const data = await res.json();
+    console.log(data);
+    if(data.success) {
+      dispatch(setUser(data.user));
+      toast.success("Profile updated successfully");
+    }
+  }
+
   return (
     <div className="profile-cont">
-      <form action="">
+      <form ref={formRef} onSubmit={handleUpdate} >
         <div className="profile-img">
-          <img src={image} alt="" />
+          <img src={user.profilePhoto} alt="profile" />
           <label htmlFor="upload">
             <span className="material-symbols-outlined">
               photo_camera
             </span>
-            <input type="file" name="upload" id="upload" />
+            <input type="file" name="profilePhoto" id="upload" />
           </label>
         </div>
         <div className="name">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input type="text" defaultValue={user.username} name="username" id="name" />
         </div>
         <div className="email">
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
+          <input type="text" value={user.email} disabled name="email" id="email" />
         </div>
         <div className="buttons">
           <button type="reset">Reset</button>
