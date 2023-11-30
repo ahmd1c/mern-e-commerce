214e81d3114d394e0a9a140ae51d52c77d0dd20c
ahmd1c <gwerqweldiv@gmail.com>
2023-11-30 22:36:14 +0200

    added filter products functionality

--- client/src/pages/products/pageComponents/FilterProducts.jsx
+++ client/src/pages/products/pageComponents/FilterProducts.jsx
@@ -1,91 +1,139 @@
+import { useRef, useState } from "react";
+import "./filterProducts.css";
+import { useSearchParams } from "react-router-dom";
+import CategoryFilter from "./CategoryFilter";
+import SortFilter from "./SortFilter";
+import PriceFilter from "./PriceFilter";
+import { toast } from "react-toastify";
 
-import { useState } from "react"
-import "./filterProducts.css"
+// eslint-disable-next-line react/prop-types
+function FilterProducts({ setProductsList, categoryId }) {
+  const [showFilter, setShowFilter] = useState(false);
+  const [queryParam, setQueryParam] = useSearchParams({});
+  const [subCategoryId, setSubCategoryId] = useState(null)
+  const formRef = useRef(null);
 
-function FilterProducts() {
-  const [showFilter , setShowFilter]= useState(false)
+  const handleCategory = (e) => {
+    setQueryParam((queryParam) => {
+      queryParam.set("subCategory", e.target.id);
+      return queryParam;
+    });
+    setSubCategoryId(e.target.value)
+  };
+
+  const handlePrice = (e) => {
+
+    if (e.target.id === "min-price") {
+      setQueryParam((queryParam) => {
+        queryParam.set("minPrice", e.target.value);
+        return queryParam;
+      });
+
+    } else {
+      setQueryParam((queryParam) => {
+        queryParam.set("maxPrice", e.target.value);
+        return queryParam;
+      });
+    }
+  };
+
+  const handleSortBy = (e) => {
+    setQueryParam((queryParam) => {
+      queryParam.set("sort", e.target.value);
+      return queryParam;
+    });
+  };
+
+  const hanldeApply = async (e) => {
+    e.preventDefault();
+    if (!categoryId) return toast.warning("please select the main category first");
+
+    const url = new URL(`http://localhost:5000/api/v1/product?category=${categoryId}`);
+    const searchParams = new URLSearchParams(queryParam);
+    if (searchParams.get("subCategory")) {
+      searchParams.set("subCategory", subCategoryId)
+    }
+    url.search = searchParams;
+
+    const res = await fetch(url);
+    if (!res.ok) return toast.error("something went wrong , please try again");
+    const data = await res.json();
+    setProductsList(data.products);
+  };
+
+  const handleReset = async (e) => {
+    e.preventDefault();
+    let data;
+
+    if (!categoryId) {
+        const res = await fetch(`http://localhost:5000/api/v1/product`);
+        if (!res.ok) return toast.error("something went wrong , please try again");
+        data = await res.json();
+
+    } else {
+        const res = await fetch(`http://localhost:5000/api/v1/product?category=${categoryId}`);
+        if (!res.ok) return toast.error("something went wrong , please try again");
+        data = await res.json();
+    }
+
+    if (!data) return toast.error("something went wrong , please try again");
+
+    setProductsList(data.products);
+    setQueryParam((queryParam) => {
+      queryParam.delete("subCategory");
+      queryParam.delete("minPrice");
+      queryParam.delete("maxPrice");
+      queryParam.delete("sort");
+      return queryParam;
+    });
+
+    formRef.current.sort.forEach((e) => {
+      e.checked = false;
+    });
+    formRef.current.subCategory.forEach((e) => {
+      e.checked = false;
+    });
+    formRef.current.minPrice.value = "";
+    formRef.current.maxPrice.value = "";
+
+  }
+
+  console.log("rerendered");
 
   return (
     <>
-    <span style={{position : "fixed"}} onClick={()=> setShowFilter(true)} className="material-symbols-outlined">
+      <span
+        style={{ position: "fixed" }}
+        onClick={() => setShowFilter(true)}
+        className="material-symbols-outlined"
+      >
         tune
       </span>
-    <div className={`filter-cont ${showFilter ? "active" : ""}`}>
-      <span onClick={()=> setShowFilter(false)} className={`material-symbols-outlined ${showFilter ? "active" : ""}`}>
-        close
-      </span>
-      <div className="category">
-        <h2>Categories</h2>
-        <div className="filter-category-list">
-          <div className="filter-category">
-            <input type="checkbox" id="filter-category-1" />
-            <label htmlFor="filter-category-1">All</label>
-          </div>
-          <div className="filter-category">
-            <input type="checkbox" id="filter-category-2" />
-            <label htmlFor="filter-category-2">Electronics</label>
-          </div>
-          <div className="filter-category">
-            <input type="checkbox" id="filter-category-3" />
-            <label htmlFor="filter-category-3">Clothing</label>
-          </div>
-          <div className="filter-category">
-            <input type="checkbox" id="filter-category-4" />
-            <label htmlFor="filter-category-4">Food</label>
-          </div>
-          <div className="filter-category">
-            <input type="checkbox" id="filter-category-5" />
-            <label htmlFor="filter-category-5">Health</label>
-          </div>
-        </div>
-      </div>
-      <div className="price">
-        <h2>Price</h2>
-        <div className="filter-price-list">
-          <label htmlFor="min-price">Min</label>
-          <input type="number" min={0} max={10000} id="min-price" />
-          <label htmlFor="max-price">Max</label>
-          <input type="number" min={0} max={10000} id="max-price" />
-        </div>
-      </div>
-      <div className="sort-by">
-        <h2>Sort By</h2>
-        <div className="filter-sort-by-list">
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-1" />
-            <label htmlFor="filter-sort-by-1">Price: Low to High</label>
-          </div>
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-2" />
-            <label htmlFor="filter-sort-by-2">Price: High to Low</label>
-          </div>
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-3" />
-            <label htmlFor="filter-sort-by-3">Rating: Low to High</label>
-          </div>
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-4" />
-            <label htmlFor="filter-sort-by-4">Rating: High to Low</label>
-          </div>
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-5" />
-            <label htmlFor="filter-sort-by-5">Newest</label>
-          </div>
-          <div className="filter-sort-by">
-            <input type="checkbox" id="filter-sort-by-6" />
-            <label htmlFor="filter-sort-by-6">Oldest</label>
-          </div>
+      <form onReset={handleReset} onSubmit={hanldeApply} ref={formRef} className={`filter-cont ${showFilter ? "active" : ""}`}>
+        <span
+          onClick={() => setShowFilter(false)}
+          className={`material-symbols-outlined ${showFilter ? "active" : ""}`}
+        >
+          close
+        </span>
 
-        </div>
-      </div>
-      <div className="filter-buttons">
-        <button>Reset</button>
-        <button>Apply</button>
-      </div>
+        <CategoryFilter categoryId={categoryId} handleCategory={handleCategory} />
 
-    </div>
+        <PriceFilter handlePrice={handlePrice} />
+
+        <SortFilter handleSortBy={handleSortBy} />
+
+        <div className="filter-buttons">
+          <button
+            type="reset"
+          >
+            Reset
+          </button>
+          <button type="submit" >Apply</button>
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default FilterProducts
+export default FilterProducts;
