fc465f4006f4aa67eaa902a3f87199eed0e018ca
ahmd1c <gwerqweldiv@gmail.com>
2023-12-01 19:55:10 +0200

    added order page

--- client/src/pages/user/orders/Orders.jsx
+++ client/src/pages/user/orders/Orders.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react"
 import OrdersTable from "../../Admin/orders table/OrdersTable"
 import "./orders.css"
+import { ToastContainer, toast } from "react-toastify"
 
 function Orders() {
+
+  const [ ordersList , setOrdersList ] = useState([])
+
+  useEffect(() => {
+    const getUSerOrder = async () => {
+      const res = await fetch("http://localhost:5000/api/v1/user/orders", { credentials: "include" })
+      if (!res.ok) return toast.error("something went wrong , please try again")
+      const data = await res.json();
+      console.log(data);
+      setOrdersList(data.orders)
+    }
+    getUSerOrder()
+  }, [])
+
   return (
     <div className="orders-cont" >
-      <OrdersTable profile={true} />
+      <ToastContainer />
+      <OrdersTable  profile={true}  ordersList={ordersList} />
     </div>
   )
 }
