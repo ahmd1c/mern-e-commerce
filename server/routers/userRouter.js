764103366251efc27ce5987585438288ca0bdd53
ahmd1c <gwerqweldiv@gmail.com>
2023-12-07 23:49:22 +0200

    added userOrders,changePassword,updateProfile

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,11 +1,17 @@
-const { signUp, signIn, signOut } = require("../controllers/userHandler");
-const { signUpValidation, signInValidation } = require("../validation/userValidation");
+const { getUserOrders } = require("../controllers/orderHandler");
+const { signUp, signIn, signOut, updateProfile, changePassword } = require("../controllers/userHandler");
+const { verifyToken } = require("../utils/authVerification");
+const uploadPhoto = require("../utils/uploadPhoto");
+const { signUpValidation, signInValidation, updateProfileValidation, changePasswordValidation } = require("../validation/userValidation");
 
 const router = require("express").Router()
 
 router.route("/signup").post( signUpValidation , signUp)
+router.route("/profile").put(verifyToken , uploadPhoto.single("profilePhoto") , updateProfileValidation , updateProfile)
+router.route("/changePassword").patch(verifyToken , changePasswordValidation , changePassword)
 router.route("/signin").post( signInValidation , signIn)
 router.route("/signout").get(signOut)
+router.route("/orders").get(verifyToken , getUserOrders )
 
 module.exports = router;
 
