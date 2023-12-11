7125ca4d453623beb494e17f28dec06014e439f8
ahmd1c <gwerqweldiv@gmail.com>
2023-12-11 23:56:06 +0200

    fixed bug of quantity being string not number

--- server/controllers/cartHandler.js
+++ server/controllers/cartHandler.js
@@ -36,10 +36,10 @@ exports.addToCart = asyncHandler( async (req, res , next) => {
                 {
                     productId,
                     quantity,
-                    price : product.afterPrice,
+                    price : product.currentPrice,
                 }
             ],
-            totalPrice: quantity * product.afterPrice
+            totalPrice: +quantity * product.currentPrice
         })
 
     } else {
@@ -49,18 +49,18 @@ exports.addToCart = asyncHandler( async (req, res , next) => {
         );
         // IF PRODUCT EXISTS THEN INCREMENT QUANTITY & TOTAL PRICE 
         if (productExist) {
-            productExist.quantity += quantity;
-            userCart.totalPrice += quantity * product.afterPrice;
+            productExist.quantity += +quantity;
+            userCart.totalPrice += +quantity * product.currentPrice;
 
         } else {
             // IF PRODUCT DOES NOT EXIST THEN ADD THE PRODUCT TO THE CART
             userCart.productsList.push({
                 productId,
                 quantity,
-                price : product.afterPrice
+                price : product.currentPrice
             });
             
-            userCart.totalPrice += quantity * product.afterPrice;
+            userCart.totalPrice += +quantity * product.currentPrice;
         }
 
         await userCart.save();
@@ -92,8 +92,73 @@ exports.getCart = asyncHandler( async (req, res , next) => {
 
 })
 
+exports.decreaseCartItem = asyncHandler( async (req, res , next) => {
 
+    const { productId } = req.body;
 
+    const userCart = await Cart.findOne({ userId: req.user.id }).populate("productsList.productId");
+
+    if (!userCart) {
+        return res.status(404).json({
+            success: false,
+            message: "Cart not found"
+        });
+    }
+
+    let isCartUpdated = false;
+    const product = userCart.productsList.find((p) => p.productId._id.toString() === productId.toString());
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        });
+    }
+
+    if(product.quantity > 1) {
+        product.quantity--;
+        userCart.totalPrice -= product.productId.currentPrice;
+        isCartUpdated = true;
+
+    }else if(product.quantity === 1) {
+        userCart.productsList = userCart.productsList.filter((p) => p.productId._id.toString() !== productId.toString());
+        userCart.totalPrice -= product.productId.currentPrice;
+        isCartUpdated = true;
+    }
+
+    if (isCartUpdated) {
+        await Cart.updateOne({ userId:  userCart.userId }, userCart , { new: true });
+    }
+
+    return res.status(200).json({
+        success: true,
+        userCart
+    })
+
+})
+
+exports.deleteCartItem = asyncHandler( async (req, res , next) => {
+    const { productId } = req.body;
+    const userCart = await Cart.findOne({ userId: req.user.id }).populate("productsList.productId");
+    const product = userCart.productsList.find((p) => p.productId._id.toString() === productId.toString());
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    userCart.productsList = userCart.productsList.filter((p) => p.productId._id.toString() !== productId.toString());
+    userCart.totalPrice -= product.productId.currentPrice * product.quantity;
+    await userCart.save();
+    
+    return res.status(200).json({
+        success: true,
+        userCart
+    })
+
+})
 
 exports.clearCart = asyncHandler( async (req, res , next) => {
     await Cart.findOneAndDelete({ userId: req.user.id });
